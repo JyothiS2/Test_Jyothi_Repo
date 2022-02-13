7cef821f6823ceb274deb5538bd5e8ce6e3e042f
JyothiS2 <97499801+JyothiS2@users.noreply.github.com>
2022-02-13 18:19:13 -0500

    Module # 5 assignment

---
+++ Module _5 Doing Math.R
@@ -0,0 +1,15 @@
+#Assigning values to matrix A And B
+A <- matrix(1:100, nrow=10)
+B <- matrix(1:1000, nrow=10)
+#Matrix A result 
+A
+#Matrix B result 
+B
+#To find determinant of A 
+det(A)
+ #Inverse of matrix A 
+solve(A)
+##To find determinant of B
+det(B)
+##Inverse of matrix B
+solve(B)
